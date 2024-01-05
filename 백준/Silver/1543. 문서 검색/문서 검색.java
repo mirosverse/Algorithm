4038681e0ab74085924916c4d4dd38f54dc8325e
miro <90399537+zaysverse@users.noreply.github.com>
2024-01-05 16:57:29 +0900

    [Silver V] Title: 문서 검색, Time: 120 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -7,20 +7,11 @@ class Main {
         String file = br.readLine();
         String word = br.readLine();
 
-        int cnt = 0;
+        String replaced = file.replace(word, "");
+        int count = (file.length() - replaced.length()) / word.length();
 
-        for (int i = 0; i <= file.length() - word.length(); i++) {
-            for (int j = 0; j < word.length(); j++) {
-                if (file.charAt(i + j) != word.charAt(j)) break;
-                else if (j == word.length() - 1) {
-                    cnt++;
-                    i += word.length() - 1;
-                }
-            }
-        }
 
-        System.out.println(cnt);
-
+        System.out.println(count);
     }
 
 }
