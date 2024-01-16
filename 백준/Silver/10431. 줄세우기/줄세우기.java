2d5adc5b587caab3db860bdaf7628930106f729f
miro <90399537+zaysverse@users.noreply.github.com>
2024-01-16 18:41:09 +0900

    [Silver V] Title: 줄세우기, Time: 400 ms, Memory: 21960 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,9 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-// 6 2 3 7 5 1 4   6 3
-// 1 2 3 4 5 6 7
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,31 +8,22 @@ public class Main {
 
         for (int test_case = 1; test_case <= T; test_case++) {
             int[] children = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int ans = getAns(children);
+            int ans = 0;
+
+            for (int i = 1; i <= 20; i++) {
+                int cur = children[i];
+
+                for (int j = i + 1; j <= 20; j++) {
+                    if (children[j] < children[i]) ans++;
+                }
+            }
+
             System.out.println(test_case + " " + ans);
         }
 
     }
 
-    private static int getAns(int[] children) {
-        Stack<Integer> stack = new Stack<>();
-        Stack<Integer> temp = new Stack<>();
-        int ans = 0;
 
-        stack.push(children[1]);
-
-        for (int i = 2; i <= 20; i++) {
-            while (!stack.isEmpty() && stack.peek() > children[i]) {
-                temp.push(stack.pop());
-                ans++;
-            }
-            stack.push(children[i]);
-            while (!temp.isEmpty()) {
-                stack.push(temp.pop());
-            }
-        }
-        return ans;
-    }
 }
 
 
