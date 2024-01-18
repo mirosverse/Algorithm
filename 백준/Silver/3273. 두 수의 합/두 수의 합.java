298e02b2505627a8b76bb836ba6e27fd11f986dd
miro <90399537+zaysverse@users.noreply.github.com>
2024-01-18 16:00:35 +0900

    [Silver III] Title: 두 수의 합, Time: 956 ms, Memory: 26964 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int x = Integer.parseInt(br.readLine());
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] < x) continue;
+                else if (arr[i] + arr[j] == x) {
+                    ans++;
+                }
+                break;
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+}
