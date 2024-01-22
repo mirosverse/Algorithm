d33b7e8dd150929be8c202ab37cf8a262e9ffafe
miro <90399537+zaysverse@users.noreply.github.com>
2024-01-22 21:16:07 +0900

    [Bronze III] Title: ACM 호텔, Time: 156 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int test_case = 0; test_case < T; test_case++) {
+            int H = sc.nextInt();   // 호텔의 층 수
+            int W = sc.nextInt();   // 각 층의 방 수
+            int N = sc.nextInt();   // 몇 번째 손님
+
+            int floor = (N % H == 0) ? H : N % H;
+            int room = (N % H == 0) ? N / H : N / H + 1;
+
+            System.out.println(floor + String.format("%02d", room));
+        }
+    }
+
+}
