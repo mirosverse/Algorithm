811335c2f1e2e8edd3eb8144ae954244dbe7834a
miro <90399537+zaysverse@users.noreply.github.com>
2024-04-23 00:06:04 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2172 ms, Memory: 139896 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,8 +1,8 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
-public class Main {
+// acc[i][j] = acc[i][j-1] + acc[i-1][j] - acc[i-1][j-1] + arr[i][j]
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,15 +11,16 @@ public class Main {
         int[][] arr = new int[N + 1][N + 1];
         int[][] acc = new int[N + 1][N + 1];
 
+        // initialize
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
-                acc[i][j] = acc[i - 1][j] + acc[i][j - 1] + arr[i][j] - acc[i - 1][j - 1];
+                acc[i][j] = acc[i][j - 1] + acc[i - 1][j] - acc[i - 1][j - 1] + arr[i][j];
             }
         }
 
-        int ans = 0;
+        // 쿼리
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
@@ -27,10 +28,10 @@ public class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            ans = acc[x2][y2] - acc[x2][y1 - 1] - acc[x1 - 1][y2] + acc[x1 - 1][y1 - 1];
-            System.out.println(ans);
-
+            System.out.println(acc[x2][y2] - acc[x2][y1 - 1] - acc[x1 - 1][y2] + acc[x1 - 1][y1 - 1]);
         }
+
 
     }
 }
+
