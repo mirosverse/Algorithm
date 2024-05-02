5df42b5c5f70a2fc53fc01451bd0385d3d4df1e6
miro <90399537+zaysverse@users.noreply.github.com>
2024-05-03 00:50:29 +0900

    [Silver II] Title: 용돈 관리, Time: 208 ms, Memory: 20832 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -2,42 +2,52 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int[] bank;
     static int N, M;
+    static int[] cost;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        bank = new int[N];
 
+        cost = new int[N];
         for (int i = 0; i < N; i++) {
-            bank[i] = Integer.parseInt(br.readLine());
+            cost[i] = Integer.parseInt(br.readLine());
         }
 
-        int l = 1, r = N * 100000, ans = -1;
+        System.out.println(solve());
+
+    }
+
+    static int solve() {
+        int l = 0, r = N * 10000, ans = -1;
         while (l <= r) {
             int m = (l + r) / 2;
-            if (isPossible(m)) {
+            if (find(m)) {
                 ans = m;
                 r = m - 1;
-            } else l = m + 1;
-        }
-
-        System.out.println(ans);
-    }
-
-    static boolean isPossible(int money) {
-        int cnt = 0, amount = 0;
-        for (int budget : bank) {
-            if (budget > money) return false;
-            if (amount < budget) {
-                cnt++;
-                amount = money;
+            } else {
+                l = m + 1;
             }
-            amount -= budget;
         }
-        return cnt <= M;
+        return ans;
     }
+
+    // K(money)원  인출
+    static boolean find(int money) {
+        int count = 0, wallet = 0;
+        for (int i = 0; i < N; i++) {
+            if (cost[i] > wallet) {
+                wallet = money;
+                count++;
+            }
+            wallet -= cost[i];
+            if (wallet < 0) return false;
+        }
+        return count <= M;
+    }
+
+
 }
+
